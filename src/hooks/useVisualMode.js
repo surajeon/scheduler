453cc9c06f5446bb3016c8bb8767dc8e8ed4fb47
Sura Jeon <surajeon@gmail.com>
2021-06-10 20:31:45 -0700

    useVisualMode file done. Test Passed

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,22 @@
+
+import { useState } from "react";
+export default function useVisualMode(initial) {
+  // console.log(initial);
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  const transition = (newMode, replace) => {
+    if (replace) {
+      back();
+    }
+    setMode(newMode);
+    setHistory([...history, newMode]);
+  };
+  const back = () => {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1]);
+      setHistory(history);
+    }
+  };
+  return { mode, transition, back };
+}
