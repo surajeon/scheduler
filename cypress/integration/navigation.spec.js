7760719237564cf352afd24feb4ee2c50c54bea3
Sura Jeon <surajeon@gmail.com>
2021-06-16 17:38:38 -0700

    Add cypress test file for appointments: testing for booking interview, deleting interview part done.

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,15 @@
+// import cy from "cypress";
+
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+  
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected")
+  });
+});
